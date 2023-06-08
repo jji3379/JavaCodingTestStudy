90929f1696ab2baa9b5ff9b9bd00c4bc8d12112c
jji3379 <jji3379@naver.com>
2023-06-08 18:38:59 +0900

    TwoPointersAndSlidingWindow-01 두 배열 합치기

---
+++ src/TwoPointersAndSlidingWindow/MergeTwoArrays.java
@@ -0,0 +1,66 @@
+package TwoPointersAndSlidingWindow;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * 두 배열 합치기
+ * 03-01
+ */
+public class MergeTwoArrays {
+
+    public static ArrayList<Integer> solution(int n, int[] a, int m, int[] b) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        int p1 = 0; // 포인터 1
+        int p2 = 0; // 포인터 2
+
+        while (p1 < n && p2 < m) {
+            if (a[p1] < b[p2]) {
+                answer.add(a[p1]);
+                p1++;
+            } else {
+                answer.add(b[p2]);
+                p2++;
+            }
+        }
+
+        while (p1 < n) {
+            answer.add(a[p1]);
+            p1++;
+        }
+
+        while (p2 < m) {
+            answer.add(b[p2]);
+            p2++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = kb.nextInt();
+        }
+
+        int m = kb.nextInt();
+        int[] b = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            b[i] = kb.nextInt();
+        }
+
+        for (int value : solution(n, a, m, b)) {
+            System.out.print(value + " ");
+        }
+    }
+}
+/**
+ * 이런 문제는 간단하지만 투 포인터를 아는지 물어보는 문제이므로,
+ * 투 포인터로 풀어야 한다.
+ */
