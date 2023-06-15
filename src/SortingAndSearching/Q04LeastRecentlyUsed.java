b797e6e6dc83bc5f24b6ff41a9eda2bd765a9cfb
jji3379 <jji3379@naver.com>
2023-06-15 16:51:49 +0900

    SortingAndSearching-04 Least Recently Used

--- src/SortingAndSearching/Q04LeastRecentlyUsed.java
+++ src/SortingAndSearching/Q04LeastRecentlyUsed.java
@@ -23,13 +23,12 @@ public class Q04LeastRecentlyUsed {
                 for (int i = size - 1; i >= 1; i--) {
                     cache[i] = cache[i - 1];
                 }
-                cache[0] = x;
             } else { // hit
                 for (int i = pos; i >= 1; i--) {
                     cache[i] = cache[i - 1];
                 }
-                cache[0] = x;
             }
+            cache[0] = x;
         }
 
         return cache;
